bb1c511d475b76b60fe2c3cfa3443e2a4a783761
Omar B <omar.benagoub@edu-its.it>
2025-07-30 10:42:14 +0200

    C#

---
+++ Stampa100InteriPositivi/Stampa100InteriPositivi/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+ Visualizzare 100 numeri interi positivi
+*/
+
+
+for (int i=0; i<100; i++) //iterazione definita 
+    Console.WriteLine(i);
+
---
+++ Stampa100PariPositivi/Stampa100PariPositivi/Program.cs
@@ -0,0 +1,5 @@
+﻿// visualizzare 100 numeri pari positivi
+// 0 2 4 ... 198
+
+for(int i = 0; i<100; i++)
+    Console.WriteLine(2 * i);
