6515c9224dd2006f60fbc57a3dba3dfd49da675c
Omar B <omar.benagoub@edu-its.it>
2025-07-30 10:32:04 +0200

    C#

---
+++ CharToInt/CharToInt/Program.cs
@@ -0,0 +1,11 @@
+﻿// Dato in input un carattere rappresentabile
+// visualizzare la sua posizione in ASCII standard
+
+
+
+Console.WriteLine("Inserisci un carattere da tastiera: ");
+
+Char c = char.Parse(Console.ReadLine());
+
+int posizione = (int)c;
+Console.WriteLine("Il carattere {0} e' in posizione ASCII: {1}", c,posizione);
---
+++ Somma2interi/Somma2interi/Program.cs
@@ -0,0 +1,18 @@
+﻿// Dati in input due numeri interi,
+// calcolare la somma
+// visualizzare il risultato
+
+//Fase di input
+
+Console.Write("Inserisci un numero intero: ");
+string tmp = Console.ReadLine();  //"4" esempio input, readline legge l'input
+int a = int.Parse(tmp);  // "4" diventa 4, converte in intero il relativo numero
+
+Console.Write("Inserisci un altro numero intero: ");
+int b = int.Parse(Console.ReadLine());
+
+//Fase di calcolo
+int somma = a + b; 
+
+//Fase di output
+Console.WriteLine(somma);
