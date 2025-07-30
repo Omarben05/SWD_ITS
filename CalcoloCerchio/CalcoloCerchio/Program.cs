fd7fedc4ab7034f3dbf7adc2b6126ecf30238c8b
Omar B <omar.benagoub@edu-its.it>
2025-07-30 10:35:28 +0200

    C#

---
+++ CalcoloCerchio/CalcoloCerchio/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Dato in input il lato del cerchio,
+ * calcolare diametro circonferenza e area
+ * visualizzare i risultati
+ */
+
+
+Console.Write("Raggio: ");
+double raggio = double.Parse(Console.ReadLine());
+
+
+//Calcoli
+double diametro = raggio * 2;
+double area = raggio * raggio * Math.PI;
+double circonferenza = 2*Math.PI*raggio;
+
+
+//output nuova tecnica, stampa dettaglio
+string msg = $"Diametro: {diametro}" +
+    $"\nCirconferenza: {circonferenza}" +
+    $"\nArea: {area}";
+
+Console.WriteLine(msg);
+
+
+//output nuova tecnica, stampa lineare
+string msg2 = $"Diametro: {diametro}" +
+    $", Circonferenza: {circonferenza}" +
+    $", Area: {area}";
+
+Console.WriteLine(msg2);
---
+++ CalcoloPiastrella/CalcoloPiastrella/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Dato in input il lato del quadrato,
+ * calcolare perimetro area e diagonale
+ * visualizzare i risultati
+ */
+
+
+Console.Write("Lato: ");
+double lato= double.Parse(Console.ReadLine());
+
+
+//Calcoli
+double perimetro = lato * 4;
+double area = lato * lato;
+double diagonale = lato * Math.Sqrt(2);
+
+
+//output
+
+Console.WriteLine("Perimetro: " + perimetro); // stile Java
+Console.WriteLine("Perimetro: {0}", area);  // stile segnaposto
+Console.WriteLine($"Diagonale: { diagonale}"); //stile interpolazione
